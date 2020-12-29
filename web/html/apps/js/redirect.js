4a0c82eaae5fe944800d510645e490374e824407
Allen546 <allen.haha@hotmail.com>
2020-12-29 20:03:50 +0800

    web

--- web/html/apps/js/redirect.js
+++ web/html/apps/js/redirect.js
@@ -1,9 +1,18 @@
 // JavaScript source code
 var redirect = function (url) {
+    alert("You are about to be redirected to " + url);
     window.location = url;
     return url;
 };
 var redirect_to_w3schools = function () {
     var url = redirect("https://www.w3schools.in/html-tutorial/history");
+    return url;
+};
+var redirect_to_khanacademy = function () {
+    var url = redirect("https://www.khanacademy.org/computing/computer-programming/html-css");
+    return url;
+};
+var redirect_to_codacademy = function () {
+    var url = redirect("https://www.codecademy.com/learn/learn-html");
     return url;
 }
